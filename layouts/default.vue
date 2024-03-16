72586fb7b369564027fdc53d5dfe05a6a378fa81
codemmk <2107228359@qq.com>
2024-03-16 19:38:50 +0800

    初始化

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app">
+    <header>header</header>
+    <main>
+      <slot />
+    </main>
+    <footer>footer</footer>
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>

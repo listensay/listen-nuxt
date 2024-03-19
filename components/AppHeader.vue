d381252b1a391aa957bce1d6bfb8daacd88aedc4
codemmk <2107228359@qq.com>
2024-03-19 21:34:36 +0800

    布局

---
+++ components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header bg-white relative">
+    <div class="header-bar absolute top-0 z-10">
+      213
+    </div>
+    <div class="header-profile relative brightness-50">
+      <img src="~/assets/images/bg.jpg" class="w-full h-64 max-md:h-52 object-cover bg-cover" alt="bg">
+    </div>
+    <div class="profle">
+
+      <div class="flex justify-end items-center">
+        <div class="mr-4 flex flex-col items-end">
+          <div class=" text-white text-xl">Milk</div>
+        </div>
+        <div>
+          <img src="~/assets/images/avatar.png" class="w-20 h-20 relative rounded-md shadow" alt="avatar">
+        </div>
+      </div>
+
+      <div class="desc mb-2 py-4 px-0 flex justify-end text-sm text-zinc-600">不要倒在黎明前的黑夜里</div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.header {
+  .profle {
+    @apply  -mt-16 mr-8;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,94 @@
 </script>
 
 <template>
-  <div class="app">
-    <header>header</header>
-    <main>
+  <div class="app min-h-[100vh] shadow-md">
+
+    <van-sticky :offset-top="0">
+      <header class="shadow-sm">
+          <AppHeader />
+        </header>
+    </van-sticky>
+
+    <main class="m-8">
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
+      <p>213</p>
       <slot />
     </main>
-    <footer>footer</footer>
+    <van-sticky :offset-bottom="0" position="bottom">
+      <footer class="bg-white p-4 border-t text-center">Milk Blog &copy; 2024</footer>
+    </van-sticky>
   </div>
 </template>
 
 <style lang="less" scoped>
 
+</style>
+
+<style lang="less">
+.app {
+  width: 620px;
+  @apply overflow-auto bg-white mx-auto;
+
+  @media screen and (max-width: 750px) {
+    width: 100%;
+  }
+}
 </style>
